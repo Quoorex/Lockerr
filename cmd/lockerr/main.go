6046d0f366b6b9803474dfcff50d35eb427a8f02
Quoorex <46283604+Quoorex@users.noreply.github.com>
2021-01-10 22:18:48 +0100

    Change startup message

--- cmd/lockerr/main.go
+++ cmd/lockerr/main.go
@@ -43,7 +43,7 @@ func Run() {
 	}
 
 	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	fmt.Println("Lockerr is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
