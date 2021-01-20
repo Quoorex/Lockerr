37ef8ba0ba58ade7e80f655146339aaad102eb83
Quoorex <46283604+Quoorex@users.noreply.github.com>
2021-01-20 09:51:16 +0100

    Handle locked users

--- lockerr/events.py
+++ lockerr/events.py
@@ -1,3 +1,6 @@
+from .statics import LOCKED_USERS, PERM_LOCKED_USERS
+
+
 def init_events(bot):
     @bot.event
     async def on_connect():
@@ -9,4 +12,22 @@ def init_events(bot):
         Runs checks to see if the user is locked and should be moved or
         left a channel and should be unlocked (when temporarely locked).
         """
-        print(member)
+        # Check if the user is locked at all.
+        if not (member in LOCKED_USERS.keys() or member in PERM_LOCKED_USERS.keys()):
+            return
+
+        # User disconnects from a channel.
+        if after.channel is None:
+            # Remove the user from the list of temp locked users.
+            LOCKED_USERS.pop(member)
+
+        # Get the locked user's channel
+        channel = None
+        if member in LOCKED_USERS.keys():
+            channel = LOCKED_USERS[member]
+        elif member in PERM_LOCKED_USERS.keys():
+            channel = PERM_LOCKED_USERS[member]
+
+        # User joined a different voice channel and has to be moved.
+        if channel is not None and after.channel.id != channel.id:
+            await member.move_to(channel, reason="User is locked by Lockerr.")
